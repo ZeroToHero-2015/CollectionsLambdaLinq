c8df12296a6f4a0fb0b812da23715efa9ae2613b
Claudiu Olteanu <olteanu.claudiu@ymail.com>
2015-04-20 23:36:06 +0300

    Add Delegate example

---
+++ Lambda/Delegate/SpecialFunctions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda.Delegate
+{
+    public delegate double PerformCalculation(double x, double y);
+
+    public class SpecialFunctions
+    {
+        public static double Sum(double val1, double val2)
+        {
+            return val1 + val2;
+        }
+
+        public static double Product(double val1, double val2)
+        {
+            return val1 * val2;
+        }
+
+        public static double Diff(double val1, double val2)
+        {
+            return val1 - val2;
+        }
+
+        public static void ExecuteFunction(PerformCalculation function, double param1, double param2)
+        {
+            double result = function(param1, param2);
+            Console.WriteLine(result);
+        }
+    }
+}
--- Lambda/Program.cs
+++ Lambda/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Lambda.Delegate;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,41 @@ namespace Lambda
 {
     class Program
     {
+        public static void DelegateExample()
+        {
+            Console.WriteLine("=====Example 1 (Delegate)===");
+
+            //Create Delegate instances
+            PerformCalculation sum_Function = new PerformCalculation(SpecialFunctions.Sum);
+            PerformCalculation prod_Function = new PerformCalculation(SpecialFunctions.Product);
+
+            double val1 = 2.0, val2 = 3.0;
+
+            //Call sum function
+            double sum_result = sum_Function(val1, val2);
+            Console.WriteLine("{0} + {1} = {2}", val1, val2, sum_result);
+
+            //Call product function
+            double prod_result = prod_Function(val1, val2);
+            Console.WriteLine("{0} * {1} = {2}", val1, val2, prod_result);
+
+            //Using sum_function reference
+            Console.Write("{0} + {1} = ", val1, val2);
+            SpecialFunctions.ExecuteFunction(sum_Function, val1, val2);
+
+            //Using product_function reference
+            Console.Write("{0} * {1} = ", val1, val2);
+            SpecialFunctions.ExecuteFunction(prod_Function, val1, val2);
+
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
+            //run Delegate example
+            DelegateExample();
+
+            Console.ReadKey();
         }
     }
 }
